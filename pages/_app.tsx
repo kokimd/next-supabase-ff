416a7dac0d3794999ca2dc9be0f3c3d8755071d5
maedakoki <mk.eng1119@gmail.com>
2022-07-09 16:46:17 +0900

    add Mantine settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 import { supabase } from '../utils/supabase';
 import useStore from '../utils/store';
 import { NotificationsProvider } from '@mantine/notifications';
+import { MantineProvider } from '@mantine/core';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const setSession = useStore((state) => state.setSession);
@@ -17,7 +18,16 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <NotificationsProvider position='top-center' limit={2}>
-      <Component {...pageProps} />
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: 'light',
+          fontFamily: 'Verdana sans-serif',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
     </NotificationsProvider>
   );
 }
