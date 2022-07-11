607d10369622e93b4754cc794a5fd16e51630f01
maedakoki <mk.eng1119@gmail.com>
2022-07-11 19:20:13 +0900

    add page

---
+++ pages/admin/participants/edit.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from 'next';
+import { AdminLayout } from '../../../components/layout/AdminLayout';
+
+const Participants: NextPage = () => {
+  return (
+    <AdminLayout title='参加者一覧'>
+      <p>page</p>
+    </AdminLayout>
+  );
+};
+
+export default Participants;
