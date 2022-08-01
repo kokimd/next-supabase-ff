32091f644d72195d56d6fb430cab523bd2b299b8
Koki <mk.eng1119@gmail.com>
2022-08-01 19:29:10 +0900

    Update index.ts

--- utils/users/index.ts
+++ utils/users/index.ts
@@ -1,8 +1,8 @@
 export const Users = {
   admin01: '管理者',
   judge01: 'Yuri Gahara',
-  judge02: 'Kuko Volpe',
-  judge03: 'Mina Ptt & TsugumiTgm',
+  judge02: 'TsugumiTgm',
+  judge03: 'Mina Ptt,
   judge04: 'Larry volk',
   judge05: 'Dandy Dandy',
 };
